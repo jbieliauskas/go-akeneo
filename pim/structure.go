128b7d75d52ec3e3f95f644397f13ccc9b4fff8c
Justas Bieliauskas <justyvas0@gmail.com>
2021-03-06 22:17:28 +0200

    Move pim options & groups to structure.go #27

--- pim/structure.go
+++ pim/structure.go
@@ -1,5 +1,12 @@
 package pim
 
+// AttributeOption is an attribute option response structure.
+type AttributeOption struct {
+	Code   string `json:"code"`
+	Ord    int    `json:"sort_order"`
+	Labels Labels `json:"labels"`
+}
+
 // AttributeGroup is an attribute group.
 type AttributeGroup struct {
 	Code   string   `json:"code"`
@@ -7,6 +14,9 @@ type AttributeGroup struct {
 	Attrs  []string `json:"attributes"`
 	Labels Labels   `json:"labels"`
 }
+
+// Labels is a JSON object that some Akeneo entities have.
+type Labels map[string]string
 
 // AddAttributes appends one or more attributes to group.
 func (group *AttributeGroup) AddAttributes(attrs ...string) {
