d5ba5200fdef850e9d0412432c18583ad735d0cd
Justas Bieliauskas <justyvas0@gmail.com>
2021-03-22 23:05:55 +0200

    Migrate all http methods to client.go #37

---
+++ pimclient/auth.go
@@ -0,0 +1,39 @@
+package pimclient
+
+import (
+	"encoding/base64"
+	"fmt"
+	"net/http"
+)
+
+// Credentials is what's used to authenticate connector and get a token.
+type Credentials struct {
+	Cid, Secret, User, Pass string
+}
+
+type token struct {
+	Access  string `json:"access_token"`
+	Refresh string `json:"refresh_token"`
+	TTL     int    `json:"expires_in"`
+}
+
+func getAccessToken(client *http.Client, url string, creds Credentials) (token, error) {
+	body := struct {
+		Username  string `json:"username"`
+		Password  string `json:"password"`
+		GrantType string `json:"grant_type"`
+	}{
+		creds.User,
+		creds.Pass,
+		"password",
+	}
+
+	req := newJSONRequest("POST", url+"/api/oauth/v1/token", body)
+	req.Header.Set("Authorization", fmt.Sprintf("Basic %s",
+		base64.StdEncoding.EncodeToString([]byte(creds.Cid+":"+creds.Secret)),
+	))
+
+	var t token
+	err := sendRequest(client, req, &t)
+	return t, err
+}
--- pimclient/client.go
+++ pimclient/client.go
@@ -1,9 +1,11 @@
 package pimclient
 
 import (
-	"encoding/base64"
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/url"
 )
 
 // PIMClient is a Pim client that users of this library interact with.
@@ -11,17 +13,6 @@ type PIMClient struct {
 	client *http.Client
 	url    string
 	token  token
-}
-
-// Credentials is what's used to authenticate connector and get a token.
-type Credentials struct {
-	Cid, Secret, User, Pass string
-}
-
-type token struct {
-	Access  string `json:"access_token"`
-	Refresh string `json:"refresh_token"`
-	TTL     int    `json:"expires_in"`
 }
 
 // New authenticates and returns PIM client
@@ -36,23 +27,81 @@ func New(url string, creds Credentials) (PIMClient, error) {
 	return PIMClient{client, url, t}, nil
 }
 
-func getAccessToken(client *http.Client, url string, creds Credentials) (token, error) {
-	body := struct {
-		Username  string `json:"username"`
-		Password  string `json:"password"`
-		GrantType string `json:"grant_type"`
-	}{
-		creds.User,
-		creds.Pass,
-		"password",
+func (c *PIMClient) list(path string, query url.Values, decodeItems func(d pageItemDecoder) interface{}) (Page, error) {
+	url := c.url + path
+	if len(query) > 0 {
+		url += "?" + query.Encode()
 	}
 
-	req := newJSONRequest("POST", url+"/api/oauth/v1/token", body)
-	req.Header.Set("Authorization", fmt.Sprintf("Basic %s",
-		base64.StdEncoding.EncodeToString([]byte(creds.Cid+":"+creds.Secret)),
-	))
+	return c.getPage(url, decodeItems)
+}
 
-	var t token
-	err := sendRequest(client, req, &t)
-	return t, err
+func (c *PIMClient) get(path string, result interface{}) error {
+	req := c.authenticate(newGetRequest(c.url + path))
+	return sendRequest(c.client, req, result)
+}
+
+func (c *PIMClient) create(path string, payload interface{}) (string, error) {
+	req := c.authenticate(newJSONRequest("POST", c.url+path, payload))
+
+	res, err := c.client.Do(req)
+	if err != nil {
+		return "", wrapFailedError()
+	}
+
+	res.Body.Close()
+
+	return res.Header.Get("Location"), nil
+}
+
+func (c *PIMClient) getPage(url string, decodeItems func(d pageItemDecoder) interface{}) (Page, error) {
+	req := c.authenticate(newGetRequest(url))
+
+	res, err := c.client.Do(req)
+	if err != nil {
+		return Page{}, wrapFailedError()
+	}
+
+	p, err := newPage(res.Body, decodeItems)
+	if err != nil {
+		return Page{}, wrapFailedError()
+	}
+
+	return p, nil
+}
+
+func (c *PIMClient) authenticate(req *http.Request) *http.Request {
+	token := fmt.Sprintf("Bearer %s", c.token.Access)
+	req.Header.Add("Authorization", token)
+	return req
+}
+
+func newGetRequest(url string) *http.Request {
+	req, _ := http.NewRequest("GET", url, nil)
+	return req
+}
+
+func newJSONRequest(method, url string, payload interface{}) *http.Request {
+	body, _ := json.Marshal(payload)
+	req, _ := http.NewRequest(method, url, bytes.NewReader(body))
+	req.Header.Set("Content-Type", "application/json")
+	return req
+}
+
+func sendRequest(client *http.Client, req *http.Request, result interface{}) error {
+	res, err := client.Do(req)
+	if err != nil {
+		return wrapFailedError()
+	}
+
+	defer res.Body.Close()
+
+	if result != nil {
+		err := json.NewDecoder(res.Body).Decode(result)
+		if err != nil {
+			return wrapFailedError()
+		}
+	}
+
+	return nil
 }
