68ecf0c09526f76eb59d68be20c44329048e6797
Justas Bieliauskas <justyvas0@gmail.com>
2021-03-27 22:22:24 +0200

    Merge groups code and options code to one source file #42

--- pim/structure.go
+++ pim/structure.go
@@ -2,11 +2,72 @@ package pim
 
 import "fmt"
 
+// AttributeGroup is an attribute group.
+type AttributeGroup struct {
+	Code   string            `json:"code"`
+	Ord    int               `json:"-"`
+	Attrs  []string          `json:"attributes,omitempty"`
+	Labels map[string]string `json:"labels,omitempty"`
+}
+
 // AttributeOption is an attribute option response structure.
 type AttributeOption struct {
 	Code   string            `json:"code"`
 	Ord    int               `json:"-"`
 	Labels map[string]string `json:"labels,omitempty"`
+}
+
+// ListAttributeGroups returns a list of all attribute groups in PIM.
+func (c *PIMClient) ListAttributeGroups() (Page, error) {
+	return c.list("/api/rest/v1/attribute-groups", nil, func(d *pageItemDecoder) interface{} {
+		gs := []AttributeGroup{}
+
+		for d.more() {
+			g := decodeAttributeGroup(d)
+			gs = append(gs, g)
+		}
+
+		return gs
+	})
+}
+
+// GetAttributeGroup gets attribute group by code.
+func (c *PIMClient) GetAttributeGroup(code string) (AttributeGroup, error) {
+	path := fmt.Sprintf("/api/rest/v1/attribute-groups/%s", code)
+	res := c.get(path)
+	g := decodeAttributeGroup(res)
+	return g, res.err
+}
+
+// CreateAttributeGroup creates a group.
+func (c *PIMClient) CreateAttributeGroup(g AttributeGroup) (string, error) {
+	var ord *int
+	if g.Ord != 0 {
+		ord = &g.Ord
+	}
+
+	return c.create("/api/rest/v1/attribute-groups", struct {
+		AttributeGroup
+		SortOrder *int `json:"sort_order,omitempty"`
+	}{
+		g,
+		ord,
+	})
+}
+
+func (c *PIMClient) UpsertAttributeGroup(g AttributeGroup) (upsertAction, error) {
+	var ord *int
+	if g.Ord != 0 {
+		ord = &g.Ord
+	}
+
+	return c.upsert("/api/rest/v1/attribute-groups", struct {
+		AttributeGroup
+		SortOrder *int `json:"sort_order,omitempty"`
+	}{
+		g,
+		ord,
+	})
 }
 
 // ListAttributeOptions lists options of an attribute.
@@ -66,6 +127,18 @@ func (c *PIMClient) UpsertAttributeOption(attr string, opt AttributeOption) (ups
 		opt,
 		ord,
 	})
+}
+
+func decodeAttributeGroup(d pimDecoder) AttributeGroup {
+	var g struct {
+		AttributeGroup
+		SortOrder int `json:"sort_order"`
+	}
+
+	d.decode(&g)
+	g.Ord = g.SortOrder + 1
+
+	return g.AttributeGroup
 }
 
 func decodeAttributeOption(d pimDecoder) AttributeOption {
